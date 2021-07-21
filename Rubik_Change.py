e798d6807b62221cdd6ab4da6742a541ad2680ee
maheravi <56476428+maheravi@users.noreply.github.com>
2021-07-21 19:34:37 +0430

    Add files via upload

--- Rubik_Change.py
+++ Rubik_Change.py
@@ -7,6 +7,6 @@ image[np.where((image == [0, 255, 255]).all(axis=2))] = [255, 0, 0]
 image[np.where((image == [255, 255, 0]).all(axis=2))] = [0, 0, 255]
 image[np.where((image == [255, 0, 255]).all(axis=2))] = [0, 255, 0]
 
-
+cv2.imwrite('Rubik.png', image)
 cv2.imshow('out', image)
 cv2.waitKey()
