f4995dfea8ff2b12a9a1cc37d0c5a0e6c2b75b23
maheravi <56476428+maheravi@users.noreply.github.com>
2021-07-21 19:34:20 +0430

    Add files via upload

---
+++ Rubik_Change.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+
+image = cv2.imread("rubix.png")
+
+image[np.where((image == [0, 255, 255]).all(axis=2))] = [255, 0, 0]
+image[np.where((image == [255, 255, 0]).all(axis=2))] = [0, 0, 255]
+image[np.where((image == [255, 0, 255]).all(axis=2))] = [0, 255, 0]
+
+
+cv2.imshow('out', image)
+cv2.waitKey()
